8b2a8ff85caee1e9ce292edbc48f09c873d7e30e
Krzysztof Urbańczyk <urbanczykkr@gmail.com>
2020-02-10 20:17:07 +0100

    Add mssql to docker-compose file

---
+++ Coyote/CoyoteNET/Retry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CoyoteNET
+{
+    internal class Retry
+    {
+        public static void Do(Action action, TimeSpan interval, int maxAttempts)
+        {
+            var exceptions = new List<Exception>();
+            while (true)
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    maxAttempts--;
+                    if (maxAttempts > 0)
+                    {
+                        exceptions.Add(ex);
+                        Console.WriteLine($"{maxAttempts} attempts left because of: {ex.Message}");
+                        Thread.Sleep(interval);
+
+                    }
+                    else
+                    {
+                        throw new AggregateException(exceptions);
+                    }
+                }
+            }
+        }
+    }
+}
